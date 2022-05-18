85f1bfa75a6bebfa1855219b99cd545c763718fe
ishank jain <21a91a0513@aec.edu.in>
2022-05-18 11:24:48 +0530

    #include<stdio.h> int main() {   int n, t, sum = 0, remainder ;
      scanf ("%d", &n);
      t = n;
     while (n>0)
       {
          remainder = n % 10;
          sum = sum *10 + remainder ;
          n = n/ 10;
          
       }
        if(t== sum)
        printf("True");
        else
        printf("False");
      return 0;
    }

--- Palindrome.c
+++ Palindrome.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
-int main() {
-  int n, reversed = 0, remainder, original;
-    
-    scanf("%d", &n);
-    original = n;
-
-    // reversed integer is stored in reversed variable
-    while (n != 0) {
-        remainder = n % 10;
-        reversed = reversed * 10 + remainder;
-        n /= 10;
-    }
-
-    // palindrome if orignal and reversed are equal
-    if (original == reversed)
-        printf("True", original);
+#include<stdio.h>
+int main()
+{
+  int n, t, sum = 0, remainder ;
+  
+  scanf ("%d", &n);
+  t = n;
+ while (n>0)
+   {
+      remainder = n % 10;
+      sum = sum *10 + remainder ;
+      n = n/ 10;
+      
+   }
+    if(t== sum)
+    printf("True");
     else
-        printf("False", original);
-
-    return 0;
+    printf("False");
+  return 0;
 }
