fdfff8383211590b05ebeca6e222c56e9f0575a1
ishank jain <21a91a0513@aec.edu.in>
2022-05-18 10:50:58 +0530

    #include<stdio.h> int main() {     int rem,sum=0,n;     scanf("%d",&n);     while(n!=0)     {         rem=n%10;         sum=sum*10+rem;         n=n/10;     }     printf("%d",sum);     return 0; }

--- reverse_of_a_numbers.c
+++ reverse_of_a_numbers.c
@@ -3,13 +3,12 @@ int main()
 {
     int rem,sum=0,n;
     scanf("%d",&n);
-    while(n != 0)
+    while(n!=0)
     {
         rem=n%10;
         sum=sum*10+rem;
         n=n/10;
     }
     printf("%d",sum);
-    
     return 0;
 }
