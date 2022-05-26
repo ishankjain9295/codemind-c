8e3d970537c80652abf3494d3bd489fe1420301a
ishank jain <21a91a0513@aec.edu.in>
2022-05-26 10:05:30 +0530

    #include<stdio.h> int main() {      int n,i,d=0,c;     scanf("%d",&n);     c=n;     for(i=1;i<=n/2;i++)     {         if(n%i==0)         {             d+=i;         }     } if(d==c) printf("True"); else printf("False"); return 0; }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,23 +1,19 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    int n,i,sum=0;
+     int n,i,d=0,c;
     scanf("%d",&n);
+    c=n;
     for(i=1;i<=n/2;i++)
     {
-       if(n%i==0)
-       {
-        sum=sum+i;
-       }
+        if(n%i==0)
+        {
+            d+=i;
+        }
     }
-    if(sum==n)
-    {
-    printf("True");
-    }
-    else
-    {
-    printf("False");
-    }
-    return 0;
+if(d==c)
+printf("True");
+else
+printf("False");
+return 0;
 }
